a9330c5a011a4b56df1b97f22d45566ee01bc161
Bobomurod <111354555+lilFrees@users.noreply.github.com>
2024-09-26 12:15:41 +0500

    asd

--- src/features/columns/components/BottomTab.tsx
+++ src/features/columns/components/BottomTab.tsx
@@ -3,9 +3,12 @@ function BottomTab() {
     <div className="mx-auto mt-5 w-full max-w-6xl px-2">
       <div className="flex items-center gap-2">
         <h2 className="text-2xl font-bold">Неоплаченные операции</h2>
-        <div className="rounded-full border border-slate-300 px-2">
+        <div className="rounded-full border border-slate-300 px-2 text-xs">
           0 операций
         </div>
+      </div>
+      <div className="flex items-center justify-center pt-10">
+        <h3 className="text-xl font-light">No data available</h3>
       </div>
     </div>
   );
